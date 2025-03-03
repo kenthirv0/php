384bd79fe12fc3dbe05238b59c179542d6a1c638
Kent Hirv <kent.hirv0@gmail.com>
2025-03-03 13:45:30 +0200

    a

--- h01.php
+++ h01.php
@@ -5,11 +5,13 @@
  * Kuupäev: 03.03.2025
  */
 
-$nimi = "Karin Eegreid";
-$synniaasta = 1995;
+$enimi = 'Karin';
+$pnimi = 'Eegreid';
+$nimi = $enimi.' '.$pnimi;
+$synniaasta = 1986;
 $tahtkuju = "Kaalud";
 
-echo "Minu nimi on $nimi, olen sündinud aastal $synniaasta ja olen $tahtkuju tähtkujust.<br>";
+echo "Minu nimi on $nimi, olen sündinud aastal $synniaasta ja olen $tahtkuju tähtkuju.<br>";
 
 echo "\"It's My Life\" - Dr. Alban<br>";
 
