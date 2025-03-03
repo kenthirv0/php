8f08bd80d4d6a3f1ac7a861fa4c7a1eb544ed439
Kent Hirv <kent.hirv0@gmail.com>
2025-03-03 14:34:42 +0200

    a

---
+++ h02.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Ülesanne 2
+ * Nimi: Kent Hirv
+ * Kuupäev: 03.03.2025
+ */
+$q = 6;
+$w = 9;
+$e = $q * $w;
+$r = $q + $w;
+$t = $q - $w;
+$y = $q / $w;
+printf('Vastus: %0.2f', $e, $r, $t, $y);
+ ?>
