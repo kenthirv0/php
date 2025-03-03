5c121ab4d0478fb5cf5dcbf1e7b4de3aa4a87391
Kent Hirv <kent.hirv0@gmail.com>
2025-03-03 11:11:42 +0200

    a

--- h01.php
+++ h01.php
@@ -1,42 +1,19 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-</head>
-    <body>
-        <div class="container">
-            <h1>Harjutus 1</h1>
-            <?php
-                /*
-                    01
-                    Kent Hirv
-                    11.02.2025
-                */
-                
-                $nimi = 'Kent';
-                $synniaasta = 2006;
-                $tahtkuju = 'veevalaja';
-                echo "mu nimi on $nimi<br> mu sünniaasta on $synniaasta<br> ma olen $tahtkuju <br>";
-                echo "“It’s My Life” – Dr. Alban<br>";
-                echo "(\(\<br>
-                ( -.-)<br>
-                o_(\")(\")<br>";
+<?php
+/**
+ * Ülesanne 1
+ * Nimi: Kent Hirv
+ * Kuupäev: 03.03.2025
+ */
 
-            ?>
-        </div>
-        <script
-            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-            crossorigin="anonymous"
-        ></script>
+$nimi = "Mati Tamm";
+$synniaasta = 1995;
+$tahtkuju = "Kaalud";
 
-        <script
-            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-            crossorigin="anonymous"
-        ></script>
-    </body>
-</html>
+echo "Minu nimi on $nimi, olen sündinud aastal $synniaasta ja olen $tahtkuju tähtkujust.<br>";
+
+echo "\"It's My Life\" - Dr. Alban<br>";
+
+echo "(\(\<br>";
+echo "( -.-)<br>";
+echo "o_(\")(\")<br>";
+?>
